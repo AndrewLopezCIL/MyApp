cce3abdb415ad9c1ad3e3d5b8081c4939c708eba
AndrewLopezCIL <45556411+AndrewLopezCIL@users.noreply.github.com>
2020-01-30 15:11:20 -0500

    Add files via upload

---
+++ Controllers/VRController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace MyApp.Controllers
+{
+    public class VRController : Controller
+    {
+        // GET: VR
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult WhereToBuy()
+        {
+            return View();
+        }
+        public ActionResult ValveIndex()
+        {
+            return View();
+        }
+        public ActionResult HTCVive()
+        {
+            return View();
+        }
+        public ActionResult OculusRiftS()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult WhereToBuy(string headsets)
+        {
+            var productType = HttpUtility.HtmlEncode(headsets);
+            var viewName = "";
+            switch (productType)
+            {
+                case "Oculus: Rift S":
+                    viewName = "OculusRiftS"; 
+                    break;
+                case "":
+                    viewName = "WhereToBuy";
+
+                    break;
+                case "HTCVive":
+                    viewName = "HTCVive"; 
+                    break;
+                case "Valve Index":
+                    viewName = "ValveIndex"; 
+                    break;  
+                default:
+                    viewName = "WhereToBuy";
+                    break;
+            }
+            return View(viewName);
+        }
+   
+    }
+}
---
+++ Models/VRHeadsetModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyApp.Models
+{
+    public class VRHeadsetModels
+    {
+        public string HeadsetName { get; set; } = "Valve Index";
+        public double HeadsetCost { get; set; } = 1600;
+        public string HeadsetStore { get; set; } = "Valve";
+    }
+}
